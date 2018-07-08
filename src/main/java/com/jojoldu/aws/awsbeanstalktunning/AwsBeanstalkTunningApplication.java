beab33d35f3bb2b01b400b3fad1a121057333449
jojoldu <jojoldu@gmail.com>
2018-07-09 08:50:49 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/aws/awsbeanstalktunning/AwsBeanstalkTunningApplication.java
@@ -0,0 +1,12 @@
+package com.jojoldu.aws.awsbeanstalktunning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBeanstalkTunningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsBeanstalkTunningApplication.class, args);
+    }
+}
