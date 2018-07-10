b1d42b5fa3b792adcaa81c5be792e965ca6b25e5
jojoldu <jojoldu@gmail.com>
2018-07-11 08:47:37 +0900

    헬스체크를 위해 / url 추가

--- src/main/java/com/jojoldu/aws/awsbeanstalktunning/Application.java
+++ src/main/java/com/jojoldu/aws/awsbeanstalktunning/Application.java
@@ -2,11 +2,19 @@ package com.jojoldu.aws.awsbeanstalktunning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello!";
     }
 }
